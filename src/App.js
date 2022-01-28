89099d0fab98167cd58452ebc970e2649dbd8b32
Hugo Perez Garcia <hugoperez80@hotmail.com>
2022-01-28 11:47:11 +0100

    formulario-example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { Form } from "./components/Form.jsx";
+import "./App.css";
+import { ProfileCard } from "./components/ProfileCard";
 
-function App() {
+
+const App = () => {
+  const [profiles, setProfiles] = useState([]);
+
+  const addProfile = (profile) => {
+    const newProfiles = [ ...profiles, profile ];
+    setProfiles(newProfiles);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Listado de perfiles</h1>
+      <Form addProfile={addProfile} />
+      <ProfileCard profiles={profiles} />
     </div>
   );
-}
+};
 
 export default App;
